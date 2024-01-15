b5716c6adb26217ae6596046a8919b46a9c83018
Alex <Alex@Pride330>
2024-01-15 20:00:43 +0100

    Initial commit

---
+++ src/java/com/servicios/UserService.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.servicios;
+
+import MODEL.User;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author Alex
+ */
+@Path("/users")
+public class UserService {
+ 
+    @GET
+    @Path("/all/xml")
+    @Produces(MediaType.APPLICATION_XML)
+    public List<User> getUsersXML(){
+         List<User> l = new ArrayList<>();
+         l.add(new User( "Alex", "Eljefe", 1993));
+         l.add(new User( "Enrique", "profesor", 1976));
+         return l;
+    }
+    
+    @GET
+    @Path("/all/json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<User> getUsersJSON(){
+         List<User> l = new ArrayList<>();
+         l.add(new User( "Alex", "Eljefe", 1993));
+         l.add(new User( "Enrique", "profesor", 1976));
+         return l;
+    }
+    
+   // @POST
+    
+}
