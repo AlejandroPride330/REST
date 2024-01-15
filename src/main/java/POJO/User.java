7a3f5fc65c7dbd83fd4c48f3d1eabf66625ad73f
Alex <Alex@Pride330>
2024-01-15 16:46:29 +0100

    Version 0

---
+++ src/main/java/POJO/User.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POJO;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Alex
+ */
+@XmlRootElement
+public class User {
+    private String id;
+    private String name;
+    private String rol;
+    private int yearBirth;
+
+    public User() {
+    }
+
+    public User(String id, String name, String rol, int yearBirth) {
+        this.id = id;
+        this.name = name;
+        this.rol = rol;
+        this.yearBirth = yearBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", name=" + name + ", rol=" + rol + ", yearBirth=" + yearBirth + '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public int getYearBirth() {
+        return yearBirth;
+    }
+
+    public void setYearBirth(int yearBirth) {
+        this.yearBirth = yearBirth;
+    }
+    
+    
+            
+}
